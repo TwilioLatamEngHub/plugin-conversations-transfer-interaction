d35aefa488bc4c3a9655317753101fec033f9543
Gus Varallo <gustavo@varallo.me>
2022-06-29 14:30:19 -0300

    feat: add Transfer button

---
+++ plugin-transfer-interaction/src/components/TransferButton/TransferButton.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { Actions } from '@twilio/flex-ui'
+
+import { Button } from '@twilio-paste/core/button'
+import { Theme } from '@twilio-paste/core/theme'
+
+const ButtonWrapper = styled.div`
+  margin: 1rem;
+  font-size: 1rem;
+  color: #fff;
+`
+
+export const TransferButton = props => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [cursor, setCursor] = useState('pointer')
+
+  return (
+    <Theme.Provider theme='default'>
+      <ButtonWrapper>
+        <Button
+          variant='reset'
+          size='reset'
+          loading={isLoading}
+          style={{ cursor: cursor }}
+          onClick={() => {
+            setIsLoading(true)
+            setCursor('not-allowed')
+            // Actions.invokeAction('ParkInteraction', { task: props.task })
+            Actions.invokeAction('ShowDirectory')
+          }}
+        >
+          Transfer
+        </Button>
+      </ButtonWrapper>
+    </Theme.Provider>
+  )
+}
